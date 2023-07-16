0022fc8f04ec992f9fb059b90e05cfbe2643852f
Saba <sabanizharadze29@gmail.com>
2023-07-16 22:22:21 +0400

    Fixed Auth Working

--- 1.js/auth.js
+++ 1.js/auth.js
@@ -1,6 +1,6 @@
 function authenticate(login, password) {
   if (login === "login" && password === "password") {
-    return "You Are Logged In";
+    return "You Are Logged In!";
   } else {
     return "Login is not correct";
   }
